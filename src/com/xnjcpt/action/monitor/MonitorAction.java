548b9c89b8294b8fdf4a995d40687d5b2ad57cee
sunyi970505 <1059747381@qq.com>
2018-06-09 13:24:59 +0800

    中文乱码

--- src/com/xnjcpt/action/monitor/MonitorAction.java
+++ src/com/xnjcpt/action/monitor/MonitorAction.java
@@ -3,7 +3,6 @@ package com.xnjcpt.action.monitor;
 import com.xnjcpt.service.monitor.MonitorService;
 
 public class MonitorAction {
-	//注入业务层的类
 	private MonitorService monitorService;
 
 	public void setMonitorService(MonitorService monitorService) {
