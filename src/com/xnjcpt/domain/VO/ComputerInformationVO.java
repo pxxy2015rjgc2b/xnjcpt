870e50ab2befd3b7f95166ebf55f5c067ddd12a1
Ex030401 <40011167+Ex030401@users.noreply.github.com>
2018-06-21 10:20:33 +0800

    sll

---
+++ src/com/xnjcpt/domain/VO/ComputerInformationVO.java
@@ -0,0 +1,51 @@
+package com.xnjcpt.domain.VO;
+
+import java.util.List;
+import com.xnjcpt.domain.DTO.UserComputerPageDTO;
+public class ComputerInformationVO {
+	private int currPage;
+	private int totalPage;
+	private int Count;
+	private int pageSize;
+	private String searchContent;
+	private List<UserComputerPageDTO> list;
+	public int getTotalPage() {
+		return totalPage;
+	}
+	public void setTotalPage(int totalPage) {
+		this.totalPage = totalPage;
+	}
+	public int getCurrPage() {
+		return currPage;
+	}
+	public void setCurrPage(int currPage) {
+		if(currPage>totalPage)
+		{this.currPage =totalPage+1;}
+		else{this.currPage = currPage;}
+	}
+	
+	public int getCount() {
+		return Count;
+	}
+	public void setCount(int count) {
+		Count = count;
+	}
+	public int getPageSize() {
+		return pageSize;
+	}
+	public void setPageSize(int pageSize) {
+		this.pageSize = pageSize;
+	}
+	public List<UserComputerPageDTO> getList() {
+		return list;
+	}
+	public void setList(List<UserComputerPageDTO> list) {
+		this.list = list;
+	}
+	public String getSearchContent() {
+		return searchContent;
+	}
+	public void setSearchContent(String searchContent) {
+		this.searchContent = searchContent;
+	}
+}
--- src/com/xnjcpt/service/impl/computer/ComputerServiceImpl.java
+++ src/com/xnjcpt/service/impl/computer/ComputerServiceImpl.java
@@ -1,14 +1,47 @@
 package com.xnjcpt.service.impl.computer;
+import java.util.List;
+
+import org.apache.poi.ss.formula.functions.T;
 
 import com.xnjcpt.dao.computer.ComputerDao;
+import com.xnjcpt.domain.DO.xnjcpt_computer;
+import com.xnjcpt.domain.DTO.UserComputerPageDTO;
+import com.xnjcpt.domain.VO.ComputerInformationVO;
 import com.xnjcpt.service.computer.ComputerService;
-
 public class ComputerServiceImpl implements ComputerService {
-	//×¢Èëdao²ãµÄÀà
+	//æ³¨å…¥daoå±‚çš„ç±»
 	private ComputerDao computerDao;
-
+	private	String user_id;
 	public void setComputerDao(ComputerDao computerDao) {
 		this.computerDao = computerDao;
 	}
+	@Override
+	public boolean saveComputer(xnjcpt_computer xc) {
+	boolean result=	computerDao.saveComputer(xc);
+	return result;
+	}
+	@Override
+	public void removeComputerById(String[] strComputerIds) {
+		computerDao.removeComputerById(strComputerIds);	
+	}
+	public ComputerInformationVO getComputerInformationByPage(ComputerInformationVO cv) {
+		int count = computerDao.getComputerCount();
+		cv.setPageSize(10);
+		cv.setCount(count);
+		cv.setTotalPage((int) Math.ceil((double) count / cv.getPageSize()));
+		return computerDao.getComputerInformationByPage(cv);
+	}
+	@Override
+	public List<UserComputerPageDTO> getComputerDetials(xnjcpt_computer xc) {
+		return computerDao.getComputerDetials(xc);
+		
+	}
+	public String getUser_id() {
+		return user_id;
+	}
+	public void setUser_id(String user_id) {
+		this.user_id = user_id;
+	}
 	
-}
+	
+	}
